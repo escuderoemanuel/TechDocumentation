2a94918250c07ac9c0d604ffe831c726d99e4d88
Emanuel Escudero <escudero.mdz@gmail.com>
2024-07-30 15:39:44 -0300

    Updated TypeScript Documentation

--- TypeScript/01.TypeOfData/21.tuplesType.ts
+++ TypeScript/01.TypeOfData/21.tuplesType.ts
@@ -1,10 +1,20 @@
-
-type State = [string, (newName: string) => void]
-
-// Ejemplo de tupla, porque siempre tiene 2 posiciones, la primera cuando se inicializa y la segunda una función
-const [hero, setHero]: State = useState('thor')
-
+// Las tuplas sirven para tener que respetar el formato de un arreglo y su longitud
 type RGB = [number, number, number]
-const text_primary: RGB = [255, 0, 0]
-const text_secondary: RGB = [255, 0, 0, 5]
-const bg_primary: RGB = ['255', 0, 0]
+let text_primary: RGB = ['23', 25, 255]
+let text_secondary: RGB = [23, 25, 255]
+// Hay un problema y es que se puede modificar mediante metodos de arrays
+text_secondary.push(16)
+console.log(text_secondary) // Esto muestra en consola -> [ 23, 25, 255, 16 ]
+
+// Para evitar esto se puede usar readonly
+type RGBa = readonly [number, number, number, number]
+let bg_primary: RGBa = [23, 25, 255, 1]
+let bg_secondary: RGBa = [23, 25, 255, 1]
+bg_primary.push(105)
+
+// Las tuplas pueden tener varios tipos de datos
+type uuid = `${string}-${string}-${string}`
+// const id: uuid = 'a4s52c'
+// const id2: uuid = 'a4s52c-a4s52c'
+// const id3: uuid = 'a4s52c-a4s52c-a4s52c'
+
