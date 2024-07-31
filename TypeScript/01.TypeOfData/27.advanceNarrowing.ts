8f33fcecf879627a5d93bcbcbcf288fe9bcbc310
Emanuel Escudero <escudero.mdz@gmail.com>
2024-07-31 11:35:50 -0300

    Updated TypeScript Documentation

--- TypeScript/01.TypeOfData/27.advanceNarrowing.ts
+++ TypeScript/01.TypeOfData/27.advanceNarrowing.ts
@@ -14,12 +14,27 @@ type Character = Mario | Sonic;
 
 function play(character: Character) {
   console.log(character.name) // Se puede acceder a las propiedades comunes a ambos personajes
-  console.log(character.jump) // NO se puede acceder a las propiedades específicas de un personaje
+  // console.log(character.jump) // NO se puede acceder a las propiedades específicas de un personaje
   // Si nos aseguramos que tenemos el tipo correcto, si podremos
   if (character.company === 'Nintendo') {
-    console.log(`${character.name} is playing Mario`);
+    character.jump() // Aquí sólo llegaría si character es de tipo Mario
     return
   }
-  // Aquí sólo llegaría si character es de tipo Sonic (pues porque no sería de 'Nintendo')
-  console.log(`${character.name} is playing Sonic`);
+  character.run() // Sólo llegaría si character es Sonic (pues porque no sería de 'Nintendo')
 }
+
+
+// Si no tuviéramos la company, con 'type guard' podemos hacer una comprobación antes de acceder a las propiedades específicas de un personaje
+function checkIsSonic(character: Character): character is Sonic {
+  return (character as Sonic).run !== undefined
+}
+
+function play2(character: Character) {
+  if (checkIsSonic(character)) {
+    character.run()
+    return
+  }
+  character.jump()
+}
+
+//! Siempre que se pueda hay que evitar los type guard, aunque funciona en tiempo de ejecución
