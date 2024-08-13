06199255ee3922109e4fe42cee4e9a54c5448179
Emanuel Escudero <escudero.mdz@gmail.com>
2024-08-13 11:54:15 -0300

    Updated

--- Python/Quiz/01.collection.js
+++ Python/Quiz/01.collection.js
@@ -5,3 +5,9 @@ let b = 'List'
 let c = 'Set'
 let d = 'Tuple'
 
+// DICTIONARY: is unordered (prior to Python 3.7), changeable, and does not allow duplicate keys.
+// LIST: is ordered, changeable (mutable), and allows duplicate members.
+// SET: is unordered, unchangeable (though you can add or remove items), and does not allow duplicate members.
+// TUPLE: is ordered, unchangeable (immutable), and allows duplicate members.
+
+//* Correct Option is 'b' -> List
